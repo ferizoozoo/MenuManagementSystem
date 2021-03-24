f63ce3d6650e0cb82887475ac097d112a1a4606f
ferizoozoo <farhadzohoorz@gmail.com>
2021-03-24 23:54:47 +0430

    Add basic menu page as homepage

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,11 +1,28 @@
 import Menu from "./containers/Menu/Menu";
+import Cooks from "./containers/Menu/Cooks/Cooks";
+import Meals from "./containers/Menu/Meals/Meals";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/cooks">
+            <Cooks />
+          </Route>
+
+          <Route path="/meals">
+            <Meals />
+          </Route>
+
+          <Route path="/">
+            <Menu />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- Frontend/frontend/src/containers/Menu/Menu.js
+++ Frontend/frontend/src/containers/Menu/Menu.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-
-import Cooks from "./Cooks/Cooks";
-import Meals from "./Meals/Meals";
+import { Link } from "react-router-dom";
 
 class Menu extends Component {
   render() {
     return (
       <>
-        <Cooks />
-        <Meals />
+        <div className="row d-flex justify-content-center">
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Cooks</h5>
+                <p className="card-text">Show list of cooks</p>
+                <Link className="btn btn-primary" to="/cooks">
+                  Cooks
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Meals</h5>
+                <p className="card-text">Show list of meals</p>
+                <Link className="btn btn-primary" to="/meals">
+                  Meals
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
