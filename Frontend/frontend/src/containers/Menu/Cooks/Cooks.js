538eab35b9a2fcc5a324745ca1c70f46fd8b6c9f
ferizoozoo <farhadzohoorz@gmail.com>
2021-03-25 00:21:29 +0430

    Add tables for showing data for cooks and meals

--- Frontend/frontend/src/containers/Menu/Cooks/Cooks.js
+++ Frontend/frontend/src/containers/Menu/Cooks/Cooks.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
 class Cooks extends Component {
+  state = {
+    cooks: [{ cookId: 1, fullName: "ferizoozoo" }],
+  };
+
   render() {
     return (
-      <>
-        <h1>Cooks</h1>
-      </>
+      <div className="row">
+        <h1 className="text-center">Cooks</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">FullName</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.cooks.map((cook, index) => (
+              <tr key={cook.cookId}>
+                <th scope="row">{index + 1}</th>
+                <td>{cook.fullName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- Frontend/frontend/src/containers/Menu/Meals/Meals.js
+++ Frontend/frontend/src/containers/Menu/Meals/Meals.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 
 class Meals extends Component {
+  state = {
+    meals: [
+      {
+        mealId: 1,
+        name: "Ghormesabzi",
+        mealTime: "2021-06-21T12:30:00",
+        cook: "ferizoozoo",
+      },
+    ],
+  };
+
   render() {
     return (
-      <>
-        <h1>Meals</h1>
-      </>
+      <div className="row">
+        <h1 className="text-center">Meals</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">MealTime</th>
+              <th scope="col">Cook</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.meals.map((meal, index) => (
+              <tr key={meal.mealId}>
+                <th scope="row">{index + 1}</th>
+                <td>{meal.name}</td>
+                <td>{meal.mealTime}</td>
+                <td>{meal.cook}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
